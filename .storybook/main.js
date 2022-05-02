7c2f314dd5056f852d5524cf26fa373a90962a59
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2022-05-02 09:46:00 +0200

    prettier

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,5 @@
-module.exports = {
-  stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
-  typescript: {
-    check: true, // type-check stories during Storybook build
-  }
+export const stories = ['../stories/**/*.stories.@(ts|tsx|js|jsx|mdx)'];
+export const addons = ['@storybook/addon-links', '@storybook/addon-essentials'];
+export const typescript = {
+  check: true, // type-check stories during Storybook build
 };
