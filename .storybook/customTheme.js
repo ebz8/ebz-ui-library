5d68fc4f09c02a26f660ab6396edb9e12c333c41
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2022-05-02 09:39:15 +0200

    add custom storybook theme

---
+++ .storybook/customTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming'
+
+export default create({
+  base: 'light',
+  brandTitle: 'ebz UI library',
+  brandUrl: 'https://github.com/ebz8/',
+  brandImage: 'https://place-hold.it/350x150',
+})
