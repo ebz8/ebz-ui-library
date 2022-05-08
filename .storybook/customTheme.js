4b866e517e334f6dd01a5dd49af1385632a33a0c
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2022-05-08 17:23:55 +0200

    remove storybook

--- .storybook/customTheme.js
+++
@@ -1,8 +0,0 @@
-import { create } from '@storybook/theming'
-
-export default create({
-  base: 'light',
-  brandTitle: 'ebz UI library',
-  brandUrl: 'https://github.com/ebz8/',
-  brandImage: 'https://place-hold.it/350x150',
-})
